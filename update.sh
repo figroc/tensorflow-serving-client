2d3cbb153e24ade51a4cf5ca2c24f5d0876d506d
P Chen <figroc@gmail.com>
2025-02-13 13:55:40 +0800

    fix: go package option in protos

--- update.sh
+++ update.sh
@@ -10,6 +10,10 @@ repo=build/upstream
 goto=proto
 
 norm=('s@(tensorflow/core/)lib/core(/error_codes.proto)@\1protobuf\2@'
+#     's@(import "tensorflow)_(serving/.*.proto".*)@\1/\2@'
+      's@(option go_package = ")github.com/.*/([a-z_]+)/go(/.*)@\1\2\3@'
+      's@(option go_package = "tensorflow/core/)protobuf@\1framework@'
+      's@(option go_package = ")github.com/(tensorflow/.*)@\1\2@'
       's@(option go_package = ".*)/[a-z_]*_go_proto(".*)@\1\2@')
 norm=$(IFS=';'; echo "${norm[*]}")
 
@@ -24,12 +28,22 @@ fetchRepo serving    tensorflow_serving
 
 rm -rf ${goto}
 function importProto {( set -e
-  if [[ ! -f ${goto}/${1} ]]; then
-    for p in $(find ${repo} -path "*/${1}"); do
-      mkdir -p ${goto}/${1%/*} && echo ${1}
-      sed -E "${norm}" ${p} > ${goto}/${1}
+  local f=${1} i=${1}
+  #if [[ "${1}" == tensorflow_serving/* ]]; then
+  #  i=${i/_//}
+  #elif [[ "${1}" == tensorflow/serving/* ]]; then
+  #  f=${f/\//_}
+  #fi
+  if [[ ! -f ${goto}/${i} ]]; then
+    for p in $(find ${repo} -path "*/${f}"); do
+      mkdir -p ${goto}/${i%/*} && echo ${p}
+      local fix="${norm}"
+      if ! grep 'option go_package = "' ${p} &>/dev/null; then
+        fix+=";s@^package .*\$@&\\noption go_package = \"${i%/*}\"\;@"
+      fi
+      sed -E "${fix}" ${p} > ${goto}/${i}
     done
-    for p in $(grep -s '^import ' ${goto}/${1} \
+    for p in $(grep -s '^import ' ${goto}/${i} \
              | grep -Eo '((\w|-)+(/|\.))+proto'); do
       importProto ${p}
     done
